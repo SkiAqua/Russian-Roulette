21ee3a771930b54e07a92da89e2cdd1f088917d5
SkiAqua <devilynmarra@gmail.com>
2024-11-01 16:22:53 -0300

    Little Adjustments

--- gun.gd
+++ gun.gd
@@ -1,3 +1,4 @@
+@tool
 extends Node2D
 class_name Gun
 
@@ -8,25 +9,27 @@ class_name Gun
 @onready var body: = $Body
 @onready var barrel: = $Barrel
 
-@onready var hammer_button: = Global.hud.get_node('HammerButton')
-@onready var trigger_button: = Global.hud.get_node('TriggerButton')
-@onready var barrel_button: = Global.hud.get_node('BarrelButton')
-
-var bullets_in_the_barrel: = []
+var hammer_button: GunComponentButton
+var trigger_button: GunComponentButton
+var barrel_button: GunComponentButton
+var bullets_in_the_barrel: Array[bool] = []
 
 func _init():
 	for bullets in range(_bullet_capacity):
-		bullets_in_the_barrel.append(1)
+		bullets_in_the_barrel.append(false)
 
 func _ready():
+	hammer_button = GunComponentButton.new(hammer)
+	get_parent().get_node('Triggers').add_child(hammer_button)
+	
 	Global.hud.resized.connect(when_game_resize)
 	when_game_resize()
 
 func when_game_resize():
 	position = Global.hud.size / 2
-	hammer_button.position = hammer.position
-	trigger_button.position = trigger.position
-	barrel_button.position = barrel.position
+	#hammer_button.position = hammer.global_position
+	#trigger_button.position = trigger.global_position
+	#barrel_button.position = barrel.global_position
 	
 
 func printzin():
