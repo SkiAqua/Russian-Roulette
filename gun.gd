2ebba367d3289efc4656ae5499f13fb6e829a4e8
SkiAqua <devilynmarra@gmail.com>
2024-11-04 12:37:18 -0300

    Trigger Update

--- gun.gd
+++ gun.gd
@@ -3,19 +3,67 @@ class_name Gun
 
 @export var gun: GunResource
 
+@onready var gun_audio_player: = $AudioPlayer
+
+var trigger_activation: = 0.0:
+	set = set_trigger_activation
+
+var trigger_ready: = true
+
+var hammer_activation: = 0.0
+var hammer_ready: = false
+
+var bullets_in_cylinder: Array[bool] = []
 func _ready():
-	gun.assemble_components(self)
+	gun.assemble_components(self, get_tree().root.get_node('/root/App/Triggers'))
+	gun.trigger_button.limit = gun.trigger_button.size
+	gun.trigger_button.button_down.connect(set_trigger_ready)
+	for b in range(gun._bullets_capacity):
+		bullets_in_cylinder.append(true)
+	
 	
 	Global.hud.resized.connect(when_game_resize)
 	when_game_resize()
+
+func _physics_process(delta: float) -> void:
+	if gun.trigger_button.swipe_offset.x > 0:
+		set_trigger_activation(gun.trigger_button.swipe_offset.x/gun.trigger_button.size.x)
+
+func set_trigger_activation(value: float):
+	if not trigger_ready:
+		return
+		
+	trigger_activation = value
 	
+	if trigger_activation >= 1.0:
+		trigger_fire()
+		trigger_activation = 0.0
+		trigger_ready = false
+	
+	set_hammer_activation(trigger_activation)
+	gun.trigger.rotation = lerp(gun.trigger_clamp[0], gun.trigger_clamp[1], trigger_activation) * PI / 180
+
+func set_hammer_activation(value: float):
+	hammer_activation = value
+	
+	if hammer_activation > 1.0:
+		hammer_activation = 1.0
+		hammer_ready = true
+	
+	gun.hammer.rotation = lerp(gun.hammer_clamp[0], gun.hammer_clamp[1], value) * PI / 180
+
+func set_trigger_ready():
+	trigger_ready = true
+
+func trigger_fire():
+	if get_chambered_bullet():
+		gun_audio_player.stream = gun._fire_sound
+	gun.trigger_button.release_swipe()
+	gun_audio_player.play()
+
+func get_chambered_bullet():
+	return bullets_in_cylinder[0]
 
 func when_game_resize():
 	position = Global.hud.size / 2
-	#hammer_button.position = hammer.global_position
-	#trigger_button.position = trigger.global_position
-	#barrel_button.position = barrel.global_position
-	
-
-func printzin():
-	print('oloko')
+	gun.update_buttons_position()
--- guns/GunResource.gd
+++ guns/GunResource.gd
@@ -2,7 +2,7 @@ class_name GunResource
 extends Resource
 
 @export_range(1,12) var _bullets_capacity: = 1
-
+@export var _fire_sound: AudioStream
 @export var _body_texture: Texture2D
 @export var _barrel_position: Vector2
 @export var _hammer_position: Vector2
@@ -20,25 +20,43 @@ extends Resource
 @export var _trigger_texture: Texture2D
 @export var _trigger_offset: Vector2
 
-func assemble_components(gun_node: Node2D, control_area: Control = null):
-	var body = Sprite2D.new()
+var trigger_button: SwipeButton
+var hammer_button: SwipeButton
+var barrel_button: SwipeButton
+
+var body
+var barrel
+var hammer
+var trigger
+
+enum COMPONENT_STATES {
+	IDLE,
+	SWIPING,
+	FIRE
+}
+
+#temp
+var trigger_clamp = [0, -35]
+var hammer_clamp = [0, 30]
+func assemble_components(gun_node: Node2D, control_area: Node = null):
+	body = Sprite2D.new()
 	body.texture = _body_texture
 	body.name = 'Body'
 	body.z_index = 1
 	
-	var barrel = Sprite2D.new()
+	barrel = Sprite2D.new()
 	barrel.texture = _barrel_texture
 	barrel.position = _barrel_position
 	barrel.name = 'Barrel'
 	
-	var hammer = Sprite2D.new()
+	hammer = Sprite2D.new()
 	hammer.position = _hammer_position
 	hammer.texture = _hammer_texture
 	hammer.offset = _hammer_offset
 	
 	hammer.name = 'Hammer'
 	
-	var trigger = Sprite2D.new()
+	trigger = Sprite2D.new()
 	trigger.position = _trigger_position
 	trigger.texture = _trigger_texture
 	trigger.offset = _trigger_offset
@@ -50,5 +68,27 @@ func assemble_components(gun_node: Node2D, control_area: Control = null):
 	gun_node.add_child(hammer)
 	gun_node.add_child(trigger)
 	
-	if not control_area:
+	if control_area == null:
 		gun_node.get_parent()
+		print(control_area)
+	
+	trigger_button = SwipeButton.new()
+	hammer_button = SwipeButton.new()
+	barrel_button = SwipeButton.new()
+	
+	update_buttons_position()
+	
+	control_area.add_child(trigger_button)
+	control_area.add_child(hammer_button)
+	control_area.add_child(barrel_button)
+
+func update_buttons_position():
+	trigger_button.size = _trigger_texture.get_size()+ Vector2(40,10)
+	trigger_button.global_position = trigger.global_position + trigger.offset - trigger_button.size/2 + Vector2(-10,15)
+
+	
+	hammer_button.size = _hammer_texture.get_size()
+	hammer_button.global_position = hammer.global_position + hammer.offset - hammer_button.size/2
+	
+	barrel_button.size = _barrel_texture.get_size()
+	barrel_button.global_position = barrel.global_position + barrel.offset - barrel_button.size/2
